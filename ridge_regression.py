8399b558a6cc98c7d308391719d7a53f774b177e
zhaofeng-shu33 <616545598@qq.com>
2020-09-20 16:06:09 +0800

    add ridge regression and test

---
+++ ridge_regression.py
@@ -0,0 +1,40 @@
+import numpy as np
+from sklearn.metrics import r2_score
+
+class RidgeRegression:
+
+    def __init__(self, alpha=1.0):
+        self.alpha = alpha
+        pass
+
+    def train(self, x_train, y_train):
+        """Receive the input training data, then learn the model.
+        Inputs:
+        x_train: np.array, shape (num_samples, num_features)
+        y_train: np.array, shape (num_samples, )
+        Outputs：
+        None
+        """
+        self.w = np.zeros(x_train.shape[1])
+        # put your training code here
+
+        return
+    def fit(self, x_train, y_train):
+        # alias for train
+        self.train(x_train, y_train)
+
+    def score(self, X, y):
+        y_pred = self.predict(X)
+        return r2_score(y, y_pred)
+
+    def predict(self, x_test):
+        """Do prediction via the learned model.
+        Inputs:
+        x_test: np.array, shape (num_samples, num_features)
+        Outputs:
+        pred: np.array, shape (num_samples, )
+        """
+
+        pred = x_test.dot(self.w)
+
+        return pred
